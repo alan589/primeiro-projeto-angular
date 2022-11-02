5373df934e7d4294ec34de0c62a535704e606115
alan cavalcante <alancavalcante307@gmail.com>
2022-11-02 15:46:18 -0300

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+
+import { CursosModule } from './cursos/cursos.module';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponenteComponent,
+    SegundoComponenteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+@Component({
+  selector: "meu-primeiro-componente",
+  template: `
+    <p>meu primeiro component com angular2</p>
+  `
+})
+export class PrimeiroComponenteComponent{
+
+}
